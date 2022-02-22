43a9a67ad81f3ee2c0f05225be8139a462fe0997
KhomsiAdam <khomsiadam@gmail.com>
2022-02-22 10:17:06 +0100

    added region and center fields to user model and validation

--- src/models/User.js
+++ src/models/User.js
@@ -1,63 +1,66 @@
-/* eslint-disable*/
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
+
 const SALT_WORK_FACTOR = 10;
 
-const User = new Schema({
-  name :{
+const User = new mongoose.Schema({
+  name: {
     type: String,
     required: true,
   },
-  cin : {
+  cin: {
     type: String,
     trim: true,
     lowercase: true,
     unique: true,
-    required: true ,
+    required: true,
   },
-  age:{
+  age: {
     type: Number,
-     min: [12, 'You must be 12 years old or older'],
+    min: [12, 'You must be 12 years old or older'],
   },
-  phone : {
+  phone: {
     type: String,
     required: true,
     unique: true,
-    match:[/^[0-9]+$/,'Please fill a valid Number phone']
+    match: [/^[0-9]+$/, 'Please fill a valid Number phone'],
   },
-  zipCode : {
-   type: Number,
-   required: true,
+  zipCode: {
+    type: Number,
+    required: true,
   },
-  city : {
-      type: String,
-      required: true,
-  },
-  address : {
-   type: String,
-   required: true
-  },
-  vaccination : {
+  city: {
     type: String,
-    enum:['vaccin1', 'vaccin2','vaccin3']
-
+    required: true,
   },
-  appointment:{
+  address: {
+    type: String,
+    required: true,
+  },
+  vaccination: {
+    type: String,
+    enum: ['vaccin1', 'vaccin2', 'vaccin3'],
+  },
+  region: {
+    type: String,
+  },
+  center: {
+    type: String,
+  },
+  appointment: {
     type: Date,
-
   },
-  SideEffects :{
-    type: Boolean, 
-    default: false
+  SideEffects: {
+    type: Boolean,
+    default: false,
   },
   email: {
     type: String,
     trim: true,
     lowercase: true,
     unique: true,
-    required: false ,
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    required: false,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
   },
   password: {
     type: String,
@@ -65,36 +68,34 @@ const User = new Schema({
   },
 }, { timestamps: true });
 
-User.pre('save', function(next) {
-  var user = this;
+User.pre('save', function (next) {
+  const user = this;
 
   // only hash the password if it has been modified (or is new)
   if (!user.isModified('password')) return next();
 
   // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-      if (err) return next(err);
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
 
-      // hash the password using our new salt
-      bcrypt.hash(user.password, salt, function(err, hash) {
-          if (err) return next(err);
-          // override the cleartext password with the hashed one
-          user.password = hash;
-          next();
-      });
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
   });
 });
 /**
 * Methods
 */
-   
-User.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-      if (err) return cb(err);
-      cb(null, isMatch);
+
+User.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
   });
 };
-
-
 
 module.exports = mongoose.model('User', User);
